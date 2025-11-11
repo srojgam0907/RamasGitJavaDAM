be3343f3dbcb5c6b55a56cf85c9b6651bd2020e9
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-11 18:53:02 +0100

    Proyecto java creado desde eclipse y gestionado por terminal (ssh listo)

---
+++ src/edu/alumno/ssh/Main.java
@@ -0,0 +1,17 @@
+package edu.alumno.ssh;
+
+import java.util.Random;
+
+public class Main {
+    @SuppressWarnings("unused")
+	public static void main(String[] args) {
+        //Investiga el uso de random y genera por pantalla los números de la lotería primitiva.
+        Random random = new Random();
+
+        System.out.println("Números de la Lotería Primitiva:");
+        // Genera 5 números principales
+
+        // Número complementario
+
+    }
+}
