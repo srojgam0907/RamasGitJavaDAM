ec2e0518746cd7f4926c7d28e2e00b50d004378d
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-11 19:02:01 +0100

    Añadido el random y mensaje final

--- src/edu/alumno/ssh/Main.java
+++ src/edu/alumno/ssh/Main.java
@@ -3,15 +3,15 @@ package edu.alumno.ssh;
 import java.util.Random;
 
 public class Main {
-    @SuppressWarnings("unused")
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         //Investiga el uso de random y genera por pantalla los números de la lotería primitiva.
         Random random = new Random();
+        int numeros=random.nextInt(10000);
 
         System.out.println("Números de la Lotería Primitiva:");
-        // Genera 5 números principales
+        System.out.println(numeros);// Genera 5 números principales 
 
-        // Número complementario
+        System.out.println("¡Buena suerte en el sorteo!");
 
     }
 }
